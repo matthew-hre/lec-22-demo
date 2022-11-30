36f974544ca16b97cafb1228da215f6cf51ef57e
jpratt-mru <jpratt@mtroyal.ca>
2022-11-30 10:36:24 -0700

    Sanity sync.

---
+++ app.py
@@ -0,0 +1,8 @@
+from tally_counter import TallyCounter
+from building import Building
+
+def main() -> None:
+    tallyCounter = TallyCounter()
+    building = Building()
+
+main()
---
+++ tally_counter.py
@@ -0,0 +1,2 @@
+class TallyCounter:
+    pass
